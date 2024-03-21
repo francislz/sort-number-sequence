f084b73064514175d9db995b8240d090e516632d
franciscone-junior <franciscone.junior@ibm.com>
2024-03-21 16:14:58 -0300

    test(uat1.1): failing test

--- src/sort-sequences.test.js
+++ src/sort-sequences.test.js
@@ -2,6 +2,6 @@ const { obtainSubSequences } = require('./sort-sequences');
 
 describe('obtainSubSequences', () => {
   it('should return obtainSubSequences', () => {
-    expect(obtainSubSequences()).toBe('hello');
+    expect(obtainSubSequences()).toEqual([3, 2, 1, 0]);
   });
 });
